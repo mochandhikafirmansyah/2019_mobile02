9c868e817acae8e64fd4dee836fbf64acd181506
andhika <mochandhikafirmansyah@gmail.com>
2019-09-13 13:12:56 +0700

    Menambahkan Override dan tugas praktikum Android Lifecycle

--- AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
+++ AndroidLifecycle/app/src/main/java/id/ac/polinema/androidlifecycle/MainActivity.java
@@ -3,6 +3,7 @@ package id.ac.polinema.androidlifecycle;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
--- TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
+++ TebakAngka/app/src/main/java/id/ac/polinema/tebakangka/MainActivity.java
@@ -11,6 +11,10 @@ import android.widget.Toast;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
+	int random = 0;
+	private EditText number_input;
+	private Button guess_button ;
+
 
 	//  TODO: deklarasikan variabel di sini
 
@@ -18,19 +22,40 @@ public class MainActivity extends AppCompatActivity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		initRandomNumber();
+		number_input = findViewById(R.id.number_input);
+		guess_button = findViewById(R.id.guess_button);
+
 		// TODO: bind layout di sini
 	}
 
 	// TODO: generate angka random di sini
-	private void initRandomNumber() {
+	private int initRandomNumber() {
+		Random r = new Random();
+		random = r.nextInt(100);
+		return random;
 
 	}
 
 	public void handleGuess(View view) {
 		// TODO: Tambahkan logika untuk melakukan pengecekan angka
+		int number = Integer.parseInt(String.valueOf(number_input.getText()));
+		if (number > random ){
+			Toast.makeText(this,"Tebakan anda terlalu besar!", Toast.LENGTH_SHORT).show();
+		}else if (number < random) {
+			Toast.makeText(this, "Tebakan anda terlalu kecil!", Toast.LENGTH_SHORT).show();
+		}else {
+			guess_button.setEnabled(false  );
+			Toast.makeText(this, "Tebakan anda benar!", Toast.LENGTH_SHORT).show();
+		}
+
 	}
+
 
 	public void handleReset(View view) {
 		// TODO: Reset tampilan
+		number_input.setText("");
+		initRandomNumber();
+		guess_button.setEnabled(true  );
 	}
 }
